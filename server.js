bd5ce22398453cc8bc9b8ca5b030690c554cc7a4
chardma3 <64539410+chardma3@users.noreply.github.com>
2023-06-15 14:53:56 +0200

    created a function for importing questions

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
-
 const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = new sqlite3.Database("./questions.db");
 
-//Enable CORS for all routes
+// Enable CORS for all routes
 app.use(cors());
 
-//API ROUTES
+// API ROUTES
 app.get("/api/questions", (req, res) => {
   db.all(`SELECT * FROM questions`, (err, rows) => {
     if (err) {
@@ -35,9 +34,36 @@ app.get("/api/questions/:level", (req, res) => {
   });
 });
 
+// Function to import questions from questions.js
+function importQuestions() {
+  const questions = require("./questions.json");
+
+  db.serialize(() => {
+    const stmt = db.prepare(
+      `INSERT INTO questions (question, option1, option2, option3, answer, level) VALUES (?,?,?,?,?,?)`
+    );
+
+    questions.forEach((question) => {
+      stmt.run(
+        question.question,
+        question.option1,
+        question.option2,
+        question.option3,
+        question.answer,
+        question.level
+      );
+    });
+
+    stmt.finalize();
+
+    console.log("Questions data imported successfully");
+  });
+}
+
 // START THE SERVER
 db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS questions (
+  db.run(
+    `CREATE TABLE IF NOT EXISTS questions (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         question TEXT,
         option1 TEXT,
@@ -45,13 +71,15 @@ db.serialize(() => {
         option3 TEXT,
         answer INTEGER,
         level INTEGER
-        )`);
+        )`,
+    () => {
+      importQuestions(); // Call the function to import questions after table creation
 
-  //INSERT YOUR QUESTIONS DATA INTO THE 'QUESTIONS' TABLE
-
-  app.listen(PORT, () => {
-    console.log(
-      `Claire's awesome food quiz API running on http://localhost:${PORT}`
-    );
-  });
+      app.listen(PORT, () => {
+        console.log(
+          `Claire's awesome food quiz API running on http://localhost:${PORT}`
+        );
+      });
+    }
+  );
 });
